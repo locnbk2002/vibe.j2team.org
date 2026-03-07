64c658d50ce4c223475e35e6950579fa778d086c
Nguyễn Quốc Hiếu <144506842+ariushieu@users.noreply.github.com>
2026-03-07 23:35:58 +0700

    feat: add github portfolio generator (#44)

---
+++ src/views/github-portfolio/meta.ts
@@ -0,0 +1,10 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'GitHub Portfolio Generator',
+  description: 'Tạo Portfolio cá nhân từ GitHub.',
+  author: 'Nguyễn Quốc Hiếu',
+  facebook: 'https://www.facebook.com/nq.hie.05',
+}
+
+export default meta
